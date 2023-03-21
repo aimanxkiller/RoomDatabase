4386aca9727cf44a7324830f408e3397a4330c22
aimanxkiller <aimanxkiller@gmail.com>
2023-03-21 15:48:07 +0800

    Update( - Updated to initialize database in main activity - Passed database instance to viewmodel in main activity )

---
+++ app/src/main/java/com/example/roomdatabase/viewmodel/UserViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.example.roomdatabase.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.roomdatabase.database.AppDatabase
+
+class UserViewModelFactory(private val myDatabase: AppDatabase):ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(UserViewModel::class.java)){
+            return UserViewModel(myDatabase.employeeDao()) as T
+        }
+        throw  IllegalArgumentException("Unknown View Model Class")
+    }
+}
